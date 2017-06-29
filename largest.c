976e2e427f27ad6de1c8ce0b30d7902777ddcda6
Kowsik14 <kowsalyakowsi78@gmail.com>
2017-06-29 12:23:11 +0530

    Create largest.c

---
+++ largest.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c;
+scanf("%d%d%d",&a,&b,&c);
+if((a>b)&&(a>c))
+printf("\n%d is larger",a);
+else if(b>c)
+printf("\n%d is larger",b);
+else
+printf("\n%d is larger",c);
+}
